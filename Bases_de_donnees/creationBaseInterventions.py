b889583949aa094cede1fc63b9443e38447e8de2
PAUL BAMMEZ <bammezpaul@gmail.com>
2021-10-06 14:00:33 +0200

    creation base intervention

--- Bases_de_donnees/creationBaseInterventions.py
+++ Bases_de_donnees/creationBaseInterventions.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-connexionBDD = sqlite3.connect("../Bases_de_donnees/EasySaves.db")
+connexionBDD = sqlite3.connect("EasySaves.db")
 curseurBDD = connexionBDD.cursor()
 
-instructionBDD = f"CREATE TABLE IF NOT EXISTS intervention("\
+instructionBDD = f"CREATE TABLE IF NOT EXISTS intervention("\ 
                 f"id_intervention INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,"\
                 f"id_intervenant INTEGER,"\
                 f"date_horaire DATE,"\
