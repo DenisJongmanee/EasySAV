0eb92f13f5092694a6abc3066a42753938eb7ccc
PAUL BAMMEZ <bammezpaul@gmail.com>
2021-10-06 16:02:56 +0200

    modif manage

--- Classes/manageIntervention.py
+++ Classes/manageIntervention.py
@@ -3,25 +3,30 @@ import sqlite3
 class ManageIntervention:
     def __init__(self):
         self.connexionBDD = sqlite3.connect("../Bases_de_donnees/EasySaves.db")
+        #connexion à la base de donnée
         self.curseurBDD = self.connexionBDD.cursor()
 
     def ajout_intervention(self, intervenant, date, adresse, modalite):
+        #methode pour ajouter une intervention
         instructionBDD = f"INSERT INTO intervention (id_intervenant, date_horaire, adresse, modalite) "\
                         f"VALUES ({intervenant}, '{date}', '{adresse}', '{modalite}')"
         self.curseurBDD.execute(instructionBDD)
+        #creation d'une intervention
         self.connexionBDD.commit()
 
     def liste_intervention(self, intervenant):
+        #methode
         instructionBDD = f"SELECT * FROM intervention WHERE id_intervenant = {intervenant}"
         self.curseurBDD.execute(instructionBDD)
         dictionnaire_retour = {}
         indexe = 1
+        # creration variable indexe = 1 correspond au numero de l'intervention
         for ligne in self.curseurBDD:
             dictionnaire_ligne = {}
             dictionnaire_ligne["intervention"] = indexe
-            dictionnaire_ligne["date_horaire"] = ligne.date_horaire
-            dictionnaire_ligne["adresse"] = ligne.adresse
-            dictionnaire_ligne["modalite"] = ligne.modalite
-            indexe += 1
+            dictionnaire_ligne["date_horaire"] = ligne[2]
+            dictionnaire_ligne["adresse"] = ligne[3]
+            dictionnaire_ligne["modalite"] = ligne[4]
+            indexe += 1 #incrémente le numero d'intervention
             dictionnaire_retour[indexe] = dictionnaire_ligne
         return dictionnaire_retour
