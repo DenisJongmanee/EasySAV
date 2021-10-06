321bf0d5cd3f393b64bab8f7c3627e5d415b2a62
Vonkuro <vonkuro@gmail.com>
2021-10-06 14:19:03 +0200

    methode pour ajouter une intervention

--- Classes/manageIntervention.py
+++ Classes/manageIntervention.py
@@ -5,8 +5,11 @@ class ManageIntervention:
         self.connexionBDD = sqlite3.connect("../Bases_de_donnees/EasySaves.db")
         self.curseurBDD = self.connexionBDD.cursor()
 
-    def ajout_intervention(self):
-        pass
+    def ajout_intervention(self, intervenant, date, adresse, modalite):
+        instructionBDD = f"INSERT INTO intervention (id_intervenant, date_horaire, addresse, modalite) "\
+                        f"VALUES ({intervenant}, {date}, {adresse}, {modalite})"
+        self.curseurBDD.execute(instructionBDD)
+        self.connexionBDD.commit()
 
-    def liste_intervention(self):
+    def liste_intervention(self, intervenant):
         pass
