aad1c84bfd6f4d1587c1db4ea3ad8e7b6c0ab4de
PAUL BAMMEZ <bammezpaul@gmail.com>
2021-10-10 22:21:16 +0200

    Commentaires

--- Classes/manageIntervention.py
+++ Classes/manageIntervention.py
@@ -2,25 +2,25 @@ import sqlite3
 
 class ManageIntervention:
     def __init__(self):
+        # connexion à la base de donnée
         self.connexionBDD = sqlite3.connect("../Bases_de_donnees/EasySaves.db")
-        #connexion à la base de donnée
         self.curseurBDD = self.connexionBDD.cursor()
 
     def ajout_intervention(self, intervenant, date, adresse, modalite):
         #methode pour ajouter une intervention
         instructionBDD = f"INSERT INTO intervention (id_intervenant, date_horaire, adresse, modalite) "\
                         f"VALUES ({intervenant}, '{date}', '{adresse}', '{modalite}')"
+        # creation d'une intervention
         self.curseurBDD.execute(instructionBDD)
-        #creation d'une intervention
         self.connexionBDD.commit()
 
     def liste_intervention(self, intervenant):
-        #methode
+        #methode qui assignent les interventions à leur intervenant
         instructionBDD = f"SELECT * FROM intervention WHERE id_intervenant = {intervenant}"
         self.curseurBDD.execute(instructionBDD)
         dictionnaire_retour = {}
+        # creation variable indexe = 1 correspond au numero de l'intervention
         indexe = 1
-        # creration variable indexe = 1 correspond au numero de l'intervention
         for ligne in self.curseurBDD:
             dictionnaire_ligne = {}
             dictionnaire_ligne["intervention"] = indexe
